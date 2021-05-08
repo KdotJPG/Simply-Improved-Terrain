6cc9567ef6bd565126f269d6dcc7b71888891225
KdotJPG <KdotJPG@users.noreply.github.com>
2021-05-08 14:31:27 -0400

    Change end biome method to Overwrite so it's accessible to other mods' mixins

--- src/main/java/jpg/k/simplyimprovedterrain/mixin/MixinEndBiomeProvider.java
+++ src/main/java/jpg/k/simplyimprovedterrain/mixin/MixinEndBiomeProvider.java
@@ -3,7 +3,6 @@ package jpg.k.simplyimprovedterrain.mixin;
 import jpg.k.simplyimprovedterrain.mixinapi.ISimplexNoiseGenerator;
 import jpg.k.simplyimprovedterrain.util.noise.MetaballEndIslandNoise;
 
-import net.minecraft.util.registry.Registry;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.provider.EndBiomeProvider;
 import net.minecraft.world.gen.SimplexNoiseGenerator;
@@ -11,15 +10,12 @@ import net.minecraft.world.gen.SimplexNoiseGenerator;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.function.Predicate;
-import java.util.function.Supplier;
-
-@Mixin(EndBiomeProvider.class)
+@Mixin(value = EndBiomeProvider.class, priority = 500)
 public class MixinEndBiomeProvider {
 
     @Shadow
@@ -39,23 +35,23 @@ public class MixinEndBiomeProvider {
 
     @Shadow
     private @Final SimplexNoiseGenerator generator;
-
-    @Inject(method = "getNoiseBiome", at = @At("HEAD"), cancellable = true)
-    private void injectGetBiomeForNoiseGen(int biomeX, int biomeY, int biomeZ, CallbackInfoReturnable<Biome> cir) {
+    
+    // Must be Overwrite for other mods (e.g. Abnormals Core) to mix into its return statements instead of only Vanilla's.
+    @Overwrite
+    public Biome getNoiseBiome(int biomeX, int biomeY, int biomeZ) {
         if ((long)biomeX * (long)biomeX + (long)biomeZ * (long)biomeZ <= 0x10000L) {
-            cir.setReturnValue(this.field_242641_i);
+            return this.field_242641_i;
         } else {
             double f = MetaballEndIslandNoise.INSTANCE.getNoise(((ISimplexNoiseGenerator)(Object)generator).getPermTable(), biomeX * 4 + 2, biomeZ * 4 + 2);
-            cir.setReturnValue(f > 40.0F?this.field_242642_j:(f >= 0.0F?this.field_242643_k:(f < -20.0F?this.field_242644_l:this.field_242645_m)));
+            return f > 40.0F?this.field_242642_j:(f >= 0.0F?this.field_242643_k:(f < -20.0F?this.field_242644_l:this.field_242645_m));
         }
-		cir.cancel();
     }
 
     @Inject(method = "func_235317_a_", at = @At("HEAD"), cancellable = true)
     private static void inject_func_235317_a_(SimplexNoiseGenerator simplex, int biomeX, int biomeZ, CallbackInfoReturnable<Float> cir) {
         float f = (float)MetaballEndIslandNoise.INSTANCE.getNoise(((ISimplexNoiseGenerator)(Object)simplex).getPermTable(), biomeX * 8, biomeZ * 8);
-    	cir.setReturnValue(f);
-		cir.cancel();
+        cir.setReturnValue(f);
+        cir.cancel();
     }
 
 }
