98450a07822f3eda515fb0abba99a835fa26f372
KdotJPG <KdotJPG@users.noreply.github.com>
2023-12-17 17:22:47 -0500

    Refactor BlueIceFeature mixin to not use Overwrite

--- common/src/main/java/jpg/k/simplyimprovedterrain/mixin/MixinBlueIceFeature.java
+++ common/src/main/java/jpg/k/simplyimprovedterrain/mixin/MixinBlueIceFeature.java
@@ -1,104 +1,74 @@
 package jpg.k.simplyimprovedterrain.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import jpg.k.simplyimprovedterrain.math.DistributionUtils;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.level.WorldGenLevel;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.feature.BlueIceFeature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
 
 /**
  * Replaces the pseudonormal-based distribution with an ellipsoid-based one.
  * Visual impact: ★★★☆☆ (when excavated)
  */
-@Mixin(value = BlueIceFeature.class, priority = 250)
+@Mixin(BlueIceFeature.class)
 public class MixinBlueIceFeature {
 
-    private static final int MAX_PLACEMENT_ATTEMPTS = 200;
     private static final float PLACEMENT_RADIUS_XZ = 4.0f * DistributionUtils.RADIUS_RATIO_SPHERE_TO_CUBE;
     private static final float PLACEMENT_RADIUS_Y = 5.5f * DistributionUtils.RADIUS_RATIO_SPHERE_TO_CUBE;
     private static final float PLACEMENT_OFFSET_Y = -0.5f;
 
-    /**
-     * @author K.jpg
-     * @reason Pseudonormal spread -> isotropic.
-     */
-    @Overwrite
-    public boolean place(FeaturePlaceContext<NoneFeatureConfiguration> featurePlaceContext) {
-        BlockPos origin = featurePlaceContext.origin();
-        WorldGenLevel worldGenLevel = featurePlaceContext.level();
+    @WrapOperation(
+            method = "place(Lnet/minecraft/world/level/levelgen/feature/FeaturePlaceContext;)Z",
+            at = @At(
+                    target = "Lnet/minecraft/core/BlockPos;offset(III)Lnet/minecraft/core/BlockPos;",
+                    value = "INVOKE"
+            )
+    )
+    private BlockPos sampleBlockOffset(BlockPos origin, int dxOriginal, int dyOriginal, int dzOriginal,
+                                       Operation<BlockPos> original, FeaturePlaceContext<NoneFeatureConfiguration> featurePlaceContext) {
         RandomSource random = featurePlaceContext.random();
 
-        if (origin.getY() > worldGenLevel.getSeaLevel() - 1) {
-            return false;
-        }
+        return DistributionUtils.sampleEllipsoidCenterBiasedSpread(
+                PLACEMENT_RADIUS_XZ, PLACEMENT_RADIUS_Y, random,
+                (dx, dy, dz) -> {
 
-        if (!worldGenLevel.getBlockState(origin).is(Blocks.WATER) && !worldGenLevel.getBlockState(origin.below()).is(Blocks.WATER)) {
-            return false;
-        }
+                    // Vanilla sampled an uneven vertical distribution. Offset for a closer resemblance.
+                    dy += PLACEMENT_OFFSET_Y;
 
-        boolean hasPackedIceAboveOrAdjacent = false;
-        for (Direction direction : Direction.values()) {
-            if (direction != Direction.DOWN && worldGenLevel.getBlockState(origin.relative(direction)).is(Blocks.PACKED_ICE)) {
-                hasPackedIceAboveOrAdjacent = true;
-                break;
-            }
-        }
+                    // Vanilla has this thing where the vertical displacement affects the XZ radius, possibly vanishingly.
+                    // Sphere/ellipsoid offset generation computes all three coordinates at once, but that doesn't mean
+                    // we can't modify the XZ coordinates retroactively.
+                    if (dy < 2) {
+                        float retroactiveRadiusXZ = PLACEMENT_RADIUS_XZ + dy / 2;
 
-        if (!hasPackedIceAboveOrAdjacent) {
-            return false;
-        }
+                        // No placement when the radius drops to or below zero.
+                        // (just return the origin, which we know has already been placed into)
+                        if (retroactiveRadiusXZ <= 0) return origin;
 
-        worldGenLevel.setBlock(origin, Blocks.BLUE_ICE.defaultBlockState(), 2);
-
-        for (int i = 0; i < MAX_PLACEMENT_ATTEMPTS; ++i) {
-
-            BlockPos placementBlockPos = DistributionUtils.sampleEllipsoidCenterBiasedSpread(
-                    PLACEMENT_RADIUS_XZ, PLACEMENT_RADIUS_Y, random,
-                    (dx, dy, dz) -> {
-
-                        // Vanilla sampled an uneven vertical distribution. Offset for a closer resemblance.
-                        dy += PLACEMENT_OFFSET_Y;
-
-                        // Vanilla has this thing where the vertical displacement affects the XZ radius, possibly vanishingly.
-                        // Sphere/ellipsoid offset generation computes all three coordinates at once, but that doesn't mean
-                        // we can't modify the XZ coordinates retroactively.
-                        if (dy < 2) {
-                            float retroactiveRadiusXZ = PLACEMENT_RADIUS_XZ + dy / 2;
-
-                            // No placement when the radius drops to or below zero.
-                            if (retroactiveRadiusXZ <= 0) return null;
-
-                            float xzScale = retroactiveRadiusXZ * (1.0f / PLACEMENT_RADIUS_XZ);
-                            dx *= xzScale;
-                            dz *= xzScale;
-                        }
-
-                        return origin.offset(Math.round(dx), Math.round(dy), Math.round(dz));
+                        float xzScale = retroactiveRadiusXZ * (1.0f / PLACEMENT_RADIUS_XZ);
+                        dx *= xzScale;
+                        dz *= xzScale;
                     }
-            );
 
-            if (placementBlockPos != null) {
-                BlockState blockStateHere = worldGenLevel.getBlockState(placementBlockPos);
-                if (blockStateHere.isAir() || blockStateHere.is(Blocks.WATER) || blockStateHere.is(Blocks.PACKED_ICE) || blockStateHere.is(Blocks.ICE)) {
-                    for (Direction direction : Direction.values()) {
-                        BlockState neighborBlockState = worldGenLevel.getBlockState(placementBlockPos.relative(direction));
-                        if (neighborBlockState.is(Blocks.BLUE_ICE)) {
-                            worldGenLevel.setBlock(placementBlockPos, Blocks.BLUE_ICE.defaultBlockState(), 2);
-                            break;
-                        }
-                    }
+                    return origin.offset(Math.round(dx), Math.round(dy), Math.round(dz));
                 }
-            }
-        }
+        );
+    }
 
-        return true;
+    @WrapOperation(
+            method = "place(Lnet/minecraft/world/level/levelgen/feature/FeaturePlaceContext;)Z",
+            at = @At(
+                    target = "Lnet/minecraft/util/RandomSource;nextInt(I)I",
+                    value = "INVOKE"
+            )
+    )
+    private int noOpRandomNextInt(RandomSource random, int range, Operation<Integer> original) {
+        return 0;
     }
 
 }
