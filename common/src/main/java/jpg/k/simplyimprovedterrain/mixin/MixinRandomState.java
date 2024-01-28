3505b85fafbec280480105386eb30293ec98bffc
KdotJPG <KdotJPG@users.noreply.github.com>
2024-01-27 23:38:57 -0500

    Improve density transform hook compatibility

--- common/src/main/java/jpg/k/simplyimprovedterrain/mixin/MixinRandomState.java
+++ common/src/main/java/jpg/k/simplyimprovedterrain/mixin/MixinRandomState.java
@@ -1,5 +1,7 @@
 package jpg.k.simplyimprovedterrain.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import jpg.k.simplyimprovedterrain.terrain.formulamodification.TerrainFormulaModification;
 import net.minecraft.core.HolderGetter;
 import net.minecraft.world.level.levelgen.NoiseGeneratorSettings;
@@ -18,12 +20,12 @@ public class MixinRandomState {
     @Shadow @Final
     private HolderGetter<NormalNoise.NoiseParameters> noises;
 
-    @Redirect(method = "<init>", at = @At(
+    @WrapOperation(method = "<init>", at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/world/level/levelgen/NoiseGeneratorSettings;noiseRouter()Lnet/minecraft/world/level/levelgen/NoiseRouter;"
     ))
-    private NoiseRouter getModifiedNoiseRouter(NoiseGeneratorSettings instance) {
-        return TerrainFormulaModification.translateFormula(instance.noiseRouter(), noises);
+    private NoiseRouter getModifiedNoiseRouter(NoiseGeneratorSettings instance, Operation<NoiseRouter> original) {
+        return TerrainFormulaModification.translateFormula(original.call(instance), noises);
     }
 
 }
